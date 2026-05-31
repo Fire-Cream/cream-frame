0ad0270f17cca46c48afe47f03c77598ca1daf49
Cream <1738678946@qq.com>
2026-05-31 22:05:09 +0800

    feat(mpj): Dynamic loading of MPJ SQL interceptor

--- cream-common/cream-common-mpj/src/main/java/com/cream/mpj/config/MyBatisPlusConfiguration.java
+++ cream-common/cream-common-mpj/src/main/java/com/cream/mpj/config/MyBatisPlusConfiguration.java
@@ -1,11 +1,12 @@
 package com.cream.mpj.config;
 
 import com.cream.mpj.inteceptor.SqlBeautyInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * MybatisPlus配置类
+ * MybatisPlus 配置类
  *
  * @author Cream
  * @since 2026-05-31 21:09
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 public class MyBatisPlusConfiguration {
 
     @Bean
+    @ConditionalOnBooleanProperty(name = {"mpj.beauty-show"}, matchIfMissing = true)
     public SqlBeautyInterceptor sqlBeautyInterceptor() {
         return  new SqlBeautyInterceptor();
     }
