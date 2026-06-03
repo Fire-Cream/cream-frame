cdef16b1a5689ac459cda2fcde468cd55bb513b1
Cream <1738678946@qq.com>
2026-06-03 20:05:05 +0800

    feat(mpj): Extract public fields of entity classes

---
+++ cream-common/cream-common-mpj/src/main/java/com/cream/mpj/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.cream.mpj.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 基础类
+ *
+ * @author Cream
+ * @since 2026-06-03 20:00
+ */
+@Data
+public class BaseEntity implements Serializable {
+
+    @TableField(fill = FieldFill.INSERT)
+    private Integer deteled;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.UPDATE)
+    private String updateBy;
+
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Integer version;
+}
--- cream-user/src/main/java/com/cream/user/entity/po/UserPo.java
+++ cream-user/src/main/java/com/cream/user/entity/po/UserPo.java
@@ -1,9 +1,9 @@
 package com.cream.user.entity.po;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.cream.mpj.entity.BaseEntity;
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
 
 /**
  * 用户 Po
@@ -13,7 +13,8 @@ import java.time.LocalDateTime;
  */
 @Data
 @TableName("user")
-public class UserPo {
+@EqualsAndHashCode(callSuper = true)
+public class UserPo extends BaseEntity {
 
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
@@ -21,22 +22,4 @@ public class UserPo {
     private String name;
 
     private Integer age;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Integer deteled;
-
-    @TableField(fill = FieldFill.INSERT)
-    private String createBy;
-
-    @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
-
-    @TableField(fill = FieldFill.UPDATE)
-    private String updateBy;
-
-    @TableField(fill = FieldFill.UPDATE)
-    private LocalDateTime updateTime;
-
-    @TableField(fill = FieldFill.INSERT)
-    private Integer version;
 }
