484a649bc5e503dbd367cdec6c1c476f1755ada1
Cream <1738678946@qq.com>
2026-06-08 22:49:29 +0800

    feat(mpj): Realize paging unified encapsulation

--- cream-common/cream-common-mpj/src/main/java/com/cream/mpj/config/MyBatisPlusConfiguration.java
+++ cream-common/cream-common-mpj/src/main/java/com/cream/mpj/config/MyBatisPlusConfiguration.java
@@ -1,5 +1,7 @@
 package com.cream.mpj.config;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.cream.mpj.inteceptor.SqlBeautyInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,15 @@ public class MyBatisPlusConfiguration {
     @Bean
     @ConditionalOnBooleanProperty(name = {"mpj.beauty-show"}, matchIfMissing = true)
     public SqlBeautyInterceptor sqlBeautyInterceptor() {
-        return  new SqlBeautyInterceptor();
+        return new SqlBeautyInterceptor();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 如果配置多个插件, 切记分页最后添加
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
     }
 }
---
+++ cream-common/cream-common-mpj/src/main/java/com/cream/mpj/entity/PageResult.java
@@ -0,0 +1,37 @@
+package com.cream.mpj.entity;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 分页返回实体
+ *
+ * @author Cream
+ * @since 2026-06-08 22:13
+ */
+@Data
+public class PageResult<T> implements Serializable {
+
+    private Long total;
+
+    private Long size;
+
+    private Long current;
+
+    private Long pages;
+
+    private List<T> records = Collections.emptyList();
+
+    public void loadData(IPage<T> records){
+       this.setCurrent(records.getCurrent());
+       this.setPages(records.getPages());
+       this.setSize(records.getSize());
+       this.setTotal(records.getTotal());
+       this.setRecords(records.getRecords());
+    }
+
+}
