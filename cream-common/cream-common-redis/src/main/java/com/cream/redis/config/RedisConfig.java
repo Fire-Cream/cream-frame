e666194d36567ac9aada620c65924496ae50d51d
Cream <1738678946@qq.com>
2026-07-11 21:47:40 +0800

    refactor(redis): add redis module

---
+++ cream-common/cream-common-redis/src/main/java/com/cream/redis/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.cream.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 配置类
+ *
+ * @author Cream
+ * @since 2026-07-11 21:00
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // Key 用 String 序列化
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
+        // Value 用 JSON 序列化
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
---
+++ cream-user/src/main/java/com/cream/user/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.cream.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author Cream
+ * @since 2026-07-11 20:46
+ */
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping
+    public void redis() {
+        redisTemplate.opsForValue().set("key", "value");
+    }
+
+}
